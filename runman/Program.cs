766e46f9ba3d4f36fe3f7deb530f27b154e3872f
Nielsen Mike tbnielse <mike.nielsen@stud.hslu.ch>
2020-10-13 16:50:57 +0200

    C# Project added
    Added Project

---
+++ runman/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Explorer700Library;
+
+namespace runman
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Explorer700 explorer700 = new Explorer700();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
