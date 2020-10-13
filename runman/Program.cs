459175747931d82a33a3c05a8566bba01d30a5d8
Nielsen Mike tbnielse <mike.nielsen@stud.hslu.ch>
2020-10-13 18:32:59 +0200

    Background
    I did it man!!

--- runman/Program.cs
+++ runman/Program.cs
@@ -1,7 +1,10 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Drawing;
 using System.IO;
 using System.Reflection;
+using System.Threading;
 using Explorer700Library;
+using Unosquare.WiringPi;
 
 namespace runman
 {
@@ -9,15 +12,24 @@ namespace runman
     {
         static void Main(string[] args)
         {
-            Stream imageStream = Assembly.GetExecutingAssembly().GetManifestResourceStream("runman.Ressources.background.png");
+            var resNames = Assembly.GetExecutingAssembly().GetManifestResourceNames();
+            
+            Stream imageStream = Assembly.GetExecutingAssembly().GetManifestResourceStream("runman.res.background.png");
             Image image = Image.FromStream(imageStream);
-            
-            
+
             Explorer700 explorer700 = new Explorer700();
+            
+            
+            if(image == null)
+                Console.WriteLine("is null");
 
             explorer700.Display.Update();
-            explorer700.Display.Graphics.DrawImage(image, new Point(128,64));
+            explorer700.Display.Graphics.DrawImage(image, new Point(0,0));
+            explorer700.Display.Graphics.DrawString("Hello",new Font(new FontFamily("arial"), 8, FontStyle.Regular), Brushes.Chocolate, new PointF(5, 50));
             explorer700.Display.Update();
+            
+            Thread.Sleep(1000);
+            explorer700.Display.Clear();
         }
     }
 }
