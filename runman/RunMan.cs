075db485e934227f0a75fa73e8928875e89ddb97
Opolor <56446631+Opolor@users.noreply.github.com>
2020-10-17 16:51:52 +0200

    Lenn Jump

--- runman/RunMan.cs
+++ runman/RunMan.cs
@@ -6,16 +6,62 @@ namespace runman
     public class RunMan : GameObject
     {
         public BoxCollider BoxCollider { get; }
-        
+        private bool jumping;
+        int ground = 15;
+        int maxHeight = 45;
+
+
         public RunMan(Point position, Image graphicImage) : base(position, graphicImage)
         {
+           
             BoxCollider = new BoxCollider(this);
+            jumping = false;
+
         }
 
         public override void Update()
         {
-            int x = Position.X + 1;
-            Position = new Point(x, Position.Y);
+          
+            if (jumping == true && Position.Y <= maxHeight)
+            {
+                int y = Position.Y + 5;
+                Position = new Point(Position.X, y);
+
+            }
+            if (jumping == true && Position.Y >= maxHeight)
+            {
+                int y = Position.Y - 5;
+                Position = new Point(Position.X, y);
+            }
+            if(jumping == true && Position.Y == ground)
+            {
+                jumping = false;
+                Console.WriteLine("Jump Comlpete");
+            }
+
+
         }
+
+        public void Jump()
+        {
+            if(jumping == true)
+            {
+                Console.WriteLine("Cannot Double Jump...");
+                return;
+            }
+            jumping = true;
+            Console.WriteLine("Jumping");
+        }
+
+
+        public void reset()
+        {
+            int x = 16;
+            int y = 15;
+            Position = new Point(x, y);
+        }
+
+
+
     }
 }
